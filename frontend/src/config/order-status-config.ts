389825c6919f888da4b4941635cb48a59c533e19
Lakshesh3817gehani <146613687+Lakshesh3817gehani@users.noreply.github.com>
2025-07-05 11:06:08 +0530

    Initial commit from downloaded project

---
+++ frontend/src/config/order-status-config.ts
@@ -0,0 +1,19 @@
+import { OrderStatus } from "@/types";
+
+type OrderStatusInfo = {
+  label: string;
+  value: OrderStatus;
+  progressValue: number;
+};
+
+export const ORDER_STATUS: OrderStatusInfo[] = [
+  { label: "Placed", value: "placed", progressValue: 0 },
+  {
+    label: "Awaiting Restaurant Confirmation",
+    value: "paid",
+    progressValue: 25,
+  },
+  { label: "In Progress", value: "inProgress", progressValue: 50 },
+  { label: "Pick Up Time", value: "outForDelivery", progressValue: 75 },
+  { label: "Order picked Up", value: "delivered", progressValue: 100 },
+];
